027f2fd14178d4009753e2101012612cf1e3443e
feergalope <fergalo.14@gmail.com>
2025-10-10 17:17:54 +0200

    fix: console warning

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,8 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 import { theme } from './theme';
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
-  
   * {
     margin: 0;
     padding: 0;
